711e331ba5750df23bf5a5c13e6313e6bf3a5864
marcodinicola88 <marco.dinicola@yahoo.it>
2023-06-17 11:59:01 +0200

    commit numero 8

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -3,6 +3,9 @@ import { AuthData } from 'src/app/auth/auth-data.interface';
 import { AuthService } from 'src/app/auth/auth.service';
 import { UserService } from 'src/app/service/user.service';
 import { User } from 'src/app/model/user.interface';
+import { Movie } from 'src/app/model/movie';
+import { MoviesService } from 'src/app/service/movies.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-user',
@@ -13,12 +16,20 @@ export class UserComponent {
 
 
   user?: User | null;
+  movies: Movie[] =[];
+  imgUrl = environment.imageURL;
 
-  constructor(private srv: UserService) {}
+  constructor(private srv: UserService, private msrv: MoviesService) {}
 
   ngOnInit(): void {
       this.srv.getUser().subscribe(user => {
         this.user = user;
+
+        this.msrv.getFavorites().subscribe(lista => {
+          this.msrv.getMovies().subscribe(movies => {
+            this.movies = movies.filter(m => lista.find(f => f.userId == user.id && f.movieId == m.id));
+          });
+      });
       });
 
   }
